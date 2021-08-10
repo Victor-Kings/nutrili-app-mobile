2d459356890cabcbca4432f29d5b760e49132682
Hiago <hiagosilvafernandes123@gmail.com>
2021-08-09 22:50:09 -0300

    add upload file from expo camera

--- src/components/ModalConfirmationFood/index.tsx
+++ src/components/ModalConfirmationFood/index.tsx
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
-import { Text,View, TouchableOpacity, Modal, KeyboardAvoidingView, Platform, ScrollView} from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  Modal,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from "react-native";
 import IconAddFood from "../../assets/img/iconAddFood.svg";
 import {
   ContainerModal,
@@ -13,7 +21,7 @@ import {
   TextHeader,
 } from "./styles";
 
-import Input from './components/Input'
+import Input from "./components/Input";
 interface IMap {
   value: string;
   i: number;
@@ -38,75 +46,77 @@ const ModalConfirmationFood = ({
   const removeItem = (value: string) => {
     const list = listFood.filter((item: string) => item !== value);
     setListFood(list);
-   
   };
-  const handlerAddFood = (food:string)=>{
+  const handlerAddFood = (food: string) => {
     listFood.push(food);
     setListFood([...listFood]);
-  }
-  const confirmationButton = ()=>{
-    if(edit) setEdit(false);
-    else handleModal()
-  }
+  };
+  const confirmationButton = () => {
+    if (edit) setEdit(false);
+    else handleModal();
+  };
   const buttonText = !edit ? (
     <TextButton size={24}>CONFIRMAR</TextButton>
   ) : (
     <TextButton size={20}>SALVAR ALTERAÇÕES</TextButton>
   );
   return (
-     
     <Modal animationType="slide" transparent={false} visible={modalOpen}>
-       <View style={{backgroundColor: 'rgba(0,0,0,0.5)'}}>
-            <ContainerModal enabled  behavior={Platform.OS === "android" ? "height" : "padding"}>
-                <ScrollView  keyboardShouldPersistTaps="handled">
-                    <TextHeader>Alimentos Encontrados:</TextHeader>
-                    <ContainerViews>
-                                {listFood.map((value: string, i: number) => (
-                                    <CardFood key={value} color="#fff">
-                                    <TextCardFood>{`\u2022 ${value}`}</TextCardFood>
-                                    {edit && (
-                                        <TouchableOpacity
-                                        style={{ backgroundColor: "black" }}
-                                        onPress={() => removeItem(value)}
-                                        >
-                                        <Text style={{ color: "red" }}>Remove</Text>
-                                        </TouchableOpacity>
-                                    )}
-                                    </CardFood>
-                                ))}
-                                {addFood && edit && <Input handlerAddFood={handlerAddFood}/>}
-                                {edit && <CardAddFood
-                                    onPress={() => {
-                                        setAddFood(true)
-                                    }}
-                                >
+      <View style={{ backgroundColor: "rgba(0,0,0,0.5)" }}>
+        <ContainerModal
+          enabled
+          behavior={Platform.OS === "android" ? "height" : "padding"}
+        >
+          <ScrollView keyboardShouldPersistTaps="handled">
+            <TextHeader>Alimentos Encontrados:</TextHeader>
+            <ContainerViews>
+              {listFood.map((value: string, i: number) => (
+                <CardFood key={value} color="#fff">
+                  <TextCardFood>{`\u2022 ${value}`}</TextCardFood>
+                  {edit && (
+                    <TouchableOpacity
+                      style={{ backgroundColor: "black" }}
+                      onPress={() => removeItem(value)}
+                    >
+                      <Text style={{ color: "red" }}>Remove</Text>
+                    </TouchableOpacity>
+                  )}
+                </CardFood>
+              ))}
+              {addFood && edit && <Input handlerAddFood={handlerAddFood} />}
+              {edit && (
+                <CardAddFood
+                  onPress={() => {
+                    setAddFood(true);
+                  }}
+                >
+                  <TextCardFood style={{ color: "#4197E5" }}>
+                    Adicionar Alimento
+                  </TextCardFood>
+                  <IconAddFood />
+                </CardAddFood>
+              )}
+            </ContainerViews>
+            <ContainerButtons>
+              <ButtonTouch color="#4197E5" onPress={() => confirmationButton()}>
+                <TextButton>{buttonText}</TextButton>
+              </ButtonTouch>
 
-                                    <TextCardFood style={{ color: "#4197E5" }}>
-                                        Adicionar Alimento
-                                    </TextCardFood>
-                                    <IconAddFood />
-                                </CardAddFood>}
-                    </ContainerViews>
-                    <ContainerButtons>
-                        <ButtonTouch color="#4197E5" onPress={() => confirmationButton()}>
-                            <TextButton>{buttonText}</TextButton>
-                        </ButtonTouch>
-
-                        <ButtonTouch
-                            color="#D93F3F"
-                            onPress={() => setEdit(true)}
-                            style={{ marginVertical: "2%" }}
-                        >
-                            <TextButton>EDITAR</TextButton>
-                        </ButtonTouch>
-                    </ContainerButtons>
-                    {/* <Image 
+              <ButtonTouch
+                color="#D93F3F"
+                onPress={() => setEdit(true)}
+                style={{ marginVertical: "2%" }}
+              >
+                <TextButton>EDITAR</TextButton>
+              </ButtonTouch>
+            </ContainerButtons>
+            {/* <Image 
                                 style={{width:'100%',height:300, borderRadius: 20}}
                                 source={{uri: capturedPhoto }}
                                 /> */}
-                </ScrollView>
-            </ContainerModal>
-        </View>
+          </ScrollView>
+        </ContainerModal>
+      </View>
     </Modal>
   );
 };
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,70 +1,93 @@
-import React,{ useState, useEffect, useRef } from "react";
-import { View, Text,TouchableOpacity, Modal, Image } from "react-native";
-import { Container, ButtonPhoto } from './styles';
-import {Camera} from 'expo-camera';
+import React, { useState, useEffect, useRef } from "react";
+import { View, Text, TouchableOpacity, Modal, Image } from "react-native";
+import { Container, ButtonPhoto } from "./styles";
+import { Camera } from "expo-camera";
 
-import MainCamera from '../../components/MainCamera';
+import MainCamera from "../../components/MainCamera";
 import ModalConfirmationFood from "../../components/ModalConfirmationFood";
+import FormData from "form-data";
+import api from "../../service/api";
 
 
-function sendImageIA({ImageData} :any){
-    //TO DO send Image to backend IA
+
+const Home = ({}) => {
+  const [hasPermission, setHasPermission] = useState<any>(null);
+  const [responseFood, setResponseFood] = useState<any>(null);
+  const camRef = useRef<any>(null);
+  const [capturedPhoto, setcapturedPhoto] = useState<any>(null);
+
+  const [modalOpen, setModalOpen] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+
+  if (hasPermission === false) {
+    return <Text>Acesso negado!</Text>;
+  }
+
+  async function takePhoto() {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      await sendImageIA(data);
+      setcapturedPhoto(data.uri);
+      setModalOpen(true);
+      console.log(data);
+    }
+  }
+
+  const handleModal = () => {
+    setModalOpen(false);
+  };
+
+  async function  sendImageIA( ImageData: any) {
+    var form = new FormData();
+
     const FoodFound = {
-        Foods: ['Arroz', 'Cenoura','Tomate','Alface','Carne Bovina','Feijão']
-    }
-    return FoodFound;
-}
-const Home = ({}) =>{
+        Foods: ["Arroz", "Cenoura", "Tomate", "Alface", "Carne Bovina", "Feijão"],
+      };
+    form.append("file1", {
+      type: "image/jpeg",
+      name: `alimento.jpg`,
+      uri: ImageData.uri,
+    });
+    await api
+    .post("/postImg", form, {
+        headers: {
+        "Content-Type": "multipart/form-data",
+        },
+    })
+    .then((apiResponse) => setResponseFood(FoodFound))
+    .catch((error)=>{
+        console.log(error)
+    })
+  }
 
-    const [hasPermission,setHasPermission] = useState<any>(null);
-    const [responseFood, setResponseFood] = useState<any>(null);
-    const camRef= useRef<any>(null);
-    const [capturedPhoto, setcapturedPhoto] = useState<any>(null);
-
-    const[modalOpen, setModalOpen] = useState(false);
-
-    useEffect(() => {
-        (async() => {
-            const{ status } = await Camera.requestPermissionsAsync();
-            setHasPermission(status === 'granted');
-        })();
-    },[]);
-
-    if(hasPermission === null){
-        return <View/>;
-    }
-
-    if(hasPermission === false){
-        return <Text>Acesso negado!</Text>
-    }
-    async function  takePhoto() {
-        if(camRef){
-            const data = await camRef.current.takePictureAsync();
-            setResponseFood(sendImageIA( data.uri ));
-            setcapturedPhoto(data.uri);
-            setModalOpen(true);
-            console.log(data);
-        }
-    }
-
-    const handleModal = ()=>{
-        setModalOpen(false)
-    }
-
-    return(
-        <Container >
-            <MainCamera camRef={camRef}/>
-            <ButtonPhoto onPress={takePhoto}>
-                <Text style={{color:'white',fontSize: 20, textAlign:'center'}}>
-                    TIRAR FOTO DA REFEIÇÃO
-                </Text>
-            </ButtonPhoto>
-            {capturedPhoto&&responseFood&&
-                <ModalConfirmationFood modalOpen={modalOpen} responseFood={responseFood.Foods} handleModal={handleModal} capturedPhoto={capturedPhoto} />
-            }
-
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <MainCamera camRef={camRef} />
+      <ButtonPhoto onPress={takePhoto}>
+        <Text style={{ color: "white", fontSize: 20, textAlign: "center" }}>
+          TIRAR FOTO DA REFEIÇÃO
+        </Text>
+      </ButtonPhoto>
+      {capturedPhoto && responseFood && (
+        <ModalConfirmationFood
+          modalOpen={modalOpen}
+          responseFood={responseFood.Foods}
+          handleModal={handleModal}
+          capturedPhoto={capturedPhoto}
+        />
+      )}
+    </Container>
+  );
+};
 
 export default Home;
