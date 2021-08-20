08b294dfb67b41408dd33d8f7321f8f40d1abd79
Hiago <hiagosilvafernandes123@gmail.com>
2021-08-19 21:31:04 -0300

    update function countdown

--- src/screens/Login/Login.tsx
+++ src/screens/Login/Login.tsx
@@ -1,43 +1,53 @@
 import React, { useState } from "react";
-import { PlaceHolder, Container, Title, ButtonTouch, TextButton, SquaresTopRight, SquaresTopLeft } from "./styles";
-import { TextInputMask } from 'react-native-masked-text'
-import Logo from "../../assets/img/LogoNutrili.svg"
-import { KeyboardAvoidingView, ScrollView } from "react-native"
+import {
+  PlaceHolder,
+  Container,
+  Title,
+  ButtonTouch,
+  TextButton,
+  SquaresTopRight,
+  SquaresTopLeft,
+} from "./styles";
+import { TextInputMask } from "react-native-masked-text";
+import Logo from "../../assets/img/LogoNutrili.svg";
+import { KeyboardAvoidingView, ScrollView } from "react-native";
 export function Login({ navigation }: any) {
-    const [number, onChangeNumber] = useState("");
-    return (
-        <KeyboardAvoidingView
-            keyboardVerticalOffset={-200}
-            behavior={"padding"}
-            style={{ flex: 1, backgroundColor: "#F6F6F6" }}
-        >
-            <ScrollView style={{ flex: 1, backgroundColor: "#F6F6F6" }}>
-                <Container>
-                    <SquaresTopLeft />
-                    <SquaresTopRight />
-                    <Logo />
-                    <Title>Insira seu telefone</Title>
+  const [number, onChangeNumber] = useState("");
+  return (
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={-200}
+      behavior={"padding"}
+      style={{ flex: 1, backgroundColor: "#F6F6F6" }}
+    >
+      <ScrollView style={{ flex: 1, backgroundColor: "#F6F6F6" }}>
+        <Container>
+          <SquaresTopLeft />
+          <SquaresTopRight />
+          <Logo />
+          <Title>Insira seu telefone</Title>
 
-                    <TextInputMask
-                        type={'cel-phone'}
-                        options={{
-                            maskType: 'BRL',
-                            withDDD: true,
-                            dddMark: '(99) '
-                        }}
-                        onChangeText={onChangeNumber}
-                        value={number}
-                        placeholder="(19) 99999-9999"
-                        style={PlaceHolder}
-                        placeholderTextColor="#C9C9C9"
-                    />
-                    <ButtonTouch color="#4197E5" onPress={() => navigation.navigate('LoginAuth')}>
-                        <TextButton>ENTRAR</TextButton>
-                    </ButtonTouch>
-                </Container>
-                {/* <SquaresBottom /> */}
-            </ScrollView>
-
-        </KeyboardAvoidingView>
-    )
+          <TextInputMask
+            type={"cel-phone"}
+            options={{
+              maskType: "BRL",
+              withDDD: true,
+              dddMark: "(99) ",
+            }}
+            onChangeText={onChangeNumber}
+            value={number}
+            placeholder="(19) 99999-9999"
+            style={PlaceHolder}
+            placeholderTextColor="#C9C9C9"
+          />
+          <ButtonTouch
+            color="#4197E5"
+            onPress={() => navigation.navigate("LoginAuth")}
+          >
+            <TextButton>ENTRAR</TextButton>
+          </ButtonTouch>
+        </Container>
+        {/* <SquaresBottom /> */}
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
--- src/screens/LoginAuth/LoginAuth.tsx
+++ src/screens/LoginAuth/LoginAuth.tsx
@@ -1,114 +1,118 @@
 import React, { useState, useEffect } from "react";
 import { TextInput, KeyboardAvoidingView, ScrollView } from "react-native";
 import {
-    Container,
-    SquaresTopRight,
-    SquaresTopLeft,
-    SquaresBottom,
-    ContentContainer,
-    Title,
-    TextContainer,
-    ButtonTouch,
-    TextButton,
-    BackButtonContainer,
-    TextBody,
-    InputCode
+  Container,
+  SquaresTopRight,
+  SquaresTopLeft,
+  SquaresBottom,
+  ContentContainer,
+  Title,
+  TextContainer,
+  ButtonTouch,
+  TextButton,
+  BackButtonContainer,
+  TextBody,
+  InputCode,
 } from "./styles";
 import { ButtonMenu } from "../../components/ButtonMenu/ButtonMenu";
 import IconBack from "../../assets/img/iconBackBlue.svg";
 
 export function LoginAuth({ ...props }: any) {
-    const [consentedSms, setConsentedSms] = useState(false)
-    const [code, onChangeCode] = useState<any>()
+  const [consentedSms, setConsentedSms] = useState(false);
+  const [code, onChangeCode] = useState<any>();
+  const [initialTime, setInitialTime] = useState(0);
+  const [startTimer, setStartTimer] = useState(false);
 
-    const handler = (page?: string) => {
-        props.navigation.navigate(page);
-    }
-    const [timer, setTimer] = React.useState(60);
-    const id: any = React.useRef(null);
+  const handler = (page?: string) => {
+    props.navigation.navigate(page);
+  };
 
-    const clear = () => {
-        window.clearInterval(id.current);
-    };
-
-    async function downTimer() {
-        id.current = window.setInterval(() => {
-            setTimer((time) => time - 1);
-        }, 1000);
-        return () => clear();
+  useEffect(() => {
+    if (initialTime > 0) {
+      setTimeout(() => {
+        setInitialTime(initialTime - 1);
+      }, 1000);
     }
 
-    useEffect(() => {
-        if (timer === 0) {
-            clear();
-            setTimer(60)
-            downTimer()
-        }
-    }, [timer]);
-
-    useEffect(() => {
-        if (code == "ABCD1234") {
-            props.navigation.navigate('LoginQuestion')
-        }
-    }, [code])
-
-    const AuthScreen = () => {
-        if (!consentedSms) {
-            return (
-                <>
-                    <BackButtonContainer>
-                        <ButtonMenu
-                            text="VOLTAR"
-                            Icon={IconBack}
-                            sizeText={18}
-                            sizeImageHeight={70}
-                            sizeImageWidth={70}
-                            handleClick={handler}
-                            page="Login"
-                        />
-                    </BackButtonContainer>
-                    <TextContainer>
-                        <Title>Vamos enviar um código por SMS para o seu telefone</Title>
-                    </TextContainer>
-                    <ButtonTouch color="#4197E5" onPress={() => { setConsentedSms(true); downTimer() }}>
-                        <TextButton>OK</TextButton>
-                    </ButtonTouch>
-                </>
-            );
-        }
-        return (
-            <KeyboardAvoidingView
-                keyboardVerticalOffset={-200}
-                behavior={"padding"}
-                style={{ flex: 1, alignItems: "center", width: "100%" }}
-            >
-                <ScrollView style={{ flex: 1, width: "100%", alignContent: "center" }}>
-                    <TextContainer>
-                        <Title>Isso pode levar{"\n"} alguns segundos...</Title>
-                        <TextBody color="#56A1E6">Insira o código</TextBody>
-                    </TextContainer>
-                    <TextInput
-                        onChangeText={onChangeCode}
-                        value={code}
-                        placeholder="CD23KIUJ"
-                        style={InputCode}
-                        placeholderTextColor="#C9C9C9"
-                    />
-                    <TextBody color="#aaaaaa" marginBottom="20px">Código será reenviado {"\n"}em {timer}s</TextBody>
-                    <TextBody color="#000000" marginBottom="20px" onPress={() => props.navigation.navigate('Login')}>Alterar telefone</TextBody>
-                </ScrollView>
-            </KeyboardAvoidingView>
-        );
+    if (initialTime === 0 && startTimer) {
+      setStartTimer(false);
     }
+  }, [initialTime, consentedSms]);
 
+  const handleOnClick = () => {
+    setInitialTime(60);
+    setConsentedSms(true);
+  };
+
+  useEffect(() => {
+    if (code == "ABCD1234") {
+      props.navigation.navigate("LoginQuestion");
+    }
+  }, [code]);
+
+  const AuthScreen = () => {
+    if (!consentedSms) {
+      return (
+        <>
+          <BackButtonContainer>
+            <ButtonMenu
+              text="VOLTAR"
+              Icon={IconBack}
+              sizeText={18}
+              sizeImageHeight={70}
+              sizeImageWidth={70}
+              handleClick={handler}
+              page="Login"
+            />
+          </BackButtonContainer>
+          <TextContainer>
+            <Title>Vamos enviar um código por SMS para o seu telefone</Title>
+          </TextContainer>
+          <ButtonTouch color="#4197E5" onPress={handleOnClick}>
+            <TextButton>OK</TextButton>
+          </ButtonTouch>
+        </>
+      );
+    }
     return (
-        <Container>
-            <SquaresTopLeft />
-            <SquaresTopRight />
-            <ContentContainer>
-                {AuthScreen()}
-            </ContentContainer>
-            <SquaresBottom />
-        </Container>
-    )
+      <KeyboardAvoidingView
+        keyboardVerticalOffset={-200}
+        behavior={"padding"}
+        style={{ flex: 1, alignItems: "center", width: "100%" }}
+      >
+        <ScrollView style={{ flex: 1, width: "100%", alignContent: "center" }}>
+          <TextContainer>
+            <Title>Isso pode levar{"\n"} alguns segundos...</Title>
+            <TextBody color="#56A1E6">Insira o código</TextBody>
+          </TextContainer>
+          <TextInput
+            onChangeText={onChangeCode}
+            value={code}
+            placeholder="CD23KIUJ"
+            style={InputCode}
+            placeholderTextColor="#C9C9C9"
+          />
+          <TextBody color="#aaaaaa" marginBottom="20px">
+            Código será reenviado {"\n"}em {initialTime}s
+          </TextBody>
+          <TextBody
+            color="#000000"
+            marginBottom="20px"
+            onPress={() => props.navigation.navigate("Login")}
+          >
+            Alterar telefone
+          </TextBody>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    );
+  };
+
+  return (
+    <Container>
+      <SquaresTopLeft />
+      <SquaresTopRight />
+      <ContentContainer>{AuthScreen()}</ContentContainer>
+      <SquaresBottom />
+    </Container>
+  );
 }
